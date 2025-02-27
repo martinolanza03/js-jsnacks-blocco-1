c163d916be29d20eb64e29707f6a5d61ac52ecec
martinolanza03 <lanzamartino3@gmail.com>
2025-02-27 12:15:41 +0100

    feat: first snacks

---
+++ Js/app.js
@@ -0,0 +1,19 @@
+// jsnack1
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+const numOne = prompt('Inserisci un numero');
+const numTwo = prompt('Inserisci un numero');
+
+if(numOne > numTwo) {
+    console.log( `Il numero maggiore è ${numOne}`);
+} else if (numOne < numTwo) {
+    console.log(`Il numero maggiore è ${numTwo}`);
+} else if (numOne === numTwo) {
+    console.log(`I numeri sono uguali ${numOne}`);
+}
+
+
+// jsnack2
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
