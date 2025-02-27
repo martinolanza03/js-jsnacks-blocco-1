e77042498e3f728e20755601c32a9076fb70d946
martinolanza03 <lanzamartino3@gmail.com>
2025-02-27 12:26:52 +0100

    feat: third snacks

--- Js/app.js
+++ Js/app.js
@@ -12,12 +12,13 @@ if(numOne > numTwo) {
 } else if (numOne === numTwo) {
     console.log(`I numeri sono uguali ${numOne}`);
 }
+
 */
 
 // jsnack2
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
-
+/*
 const wordFirst = prompt('Inserisci una parola');
 const wordSecond = prompt('Inserisci una parola');
 
@@ -28,3 +29,15 @@ if(wordFirst.length > wordSecond.length) {
 } else if (wordFirst.length === wordSecond.length) {
     console.log(`Le parole hanno la stessa lunghezza`);
 }
+
+*/
+
+// jsnack3
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+let sum = 0;
+for (i = 1; i <= 10; i++){
+    sum += parseInt(prompt('Inserisci un numero per la somma'));
+    
+}
+console.log(sum);
