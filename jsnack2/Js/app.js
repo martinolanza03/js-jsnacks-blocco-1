275d7d47f1a798e91c0abf081944d233d3ab8d64
martinolanza03 <lanzamartino3@gmail.com>
2025-02-27 12:59:00 +0100

    fix: snacks 2

--- jsnack2/Js/app.js
+++ jsnack2/Js/app.js
@@ -6,9 +6,9 @@ const wordFirst = prompt('Inserisci una parola');
 const wordSecond = prompt('Inserisci una parola');
 
 if(wordFirst.length > wordSecond.length) {
-    console.log( `La parola più lunga è ${wordFirst}`);
+    console.log( `La parola più lunga è ${wordFirst}, invece la parola più corta ${wordSecond}`);
 } else if (wordFirst.length < wordSecond.length) {
-    console.log(`La parola più lunga è ${wordSecond}`);
+    console.log(`La parola più lunga è ${wordSecond}, invece la parola più corta ${wordFirst}`);
 } else if (wordFirst.length === wordSecond.length) {
     console.log(`Le parole hanno la stessa lunghezza`);
 }
